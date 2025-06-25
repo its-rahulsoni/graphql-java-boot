762538d5822d4e20df7c4de40ba8bbdbac3e4d03
Rahul Soni <rahulsoni.myventures@gmail.com>
2025-06-25 06:26:02 +0530

    initial commit

---
+++ src/main/java/com/graphql/learning/GraphQLLearningApplication.java
@@ -0,0 +1,14 @@
+package com.graphql.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLLearningApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/graphql/learning/service/StringUtil.java
@@ -0,0 +1,9 @@
+package com.graphql.learning.service;
+
+public class StringUtil {
+
+    public String concatStrings(String str1, String str2) {
+        return str1 + str2;
+    }
+
+}
